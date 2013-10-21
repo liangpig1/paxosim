e507cde4d9bd56d8e26db5ae4043ead962ca68e4
liangpig1 <liangpig1@gmail.com>
2013-10-21 22:14:03 +0800

    basic function? not debugged yet

--- framework.js
+++ framework.js
@@ -1,7 +1,9 @@
 ﻿Node = function (id, context) {
 	this.id = id;
+	this.die = true;
 	this.ctx = context;
 	this.needToPropose = false;
+	this.recvBuffer = [];
 }
 
 Node.prototype.getId = function () {
@@ -9,9 +11,15 @@ Node.prototype.getId = function () {
 }
 
 Node.prototype.send = function (id, msg) {
+	this.ctx.fromMessage.push(new Message(this.id, id, this.getTime(), msg));
 }
 
 Node.prototype.recv = function () {
+	if (this.recvBuffer.length) {
+		return (this.recvBuffer.splice(0,1))[0];
+	} else {
+		return NULL;
+	}
 }
 
 Node.prototype.tick = function () {
@@ -25,7 +33,7 @@ Node.prototype.setStorage = function (data) {
 }
 
 Node.prototype.getTime = function () {
-	return ctx.time;
+	return this.ctx.time;
 }
 
 Node.prototype.isOnRecovery = function () {
@@ -62,16 +70,33 @@ Framework.prototype.run = function() {
 	i = 0;
 	while (i < 1000) {
 		for (j = 0; j < this.nodeId; j++) {
-			this.nodes[j].tick();
+			if (this.nodes[j].die) {
+				this.nodes[j].onRecovery();
+				this.nodes[j].die = false;
+			} else {
+				this.nodes[j].tick();
+			}
 		}
 		++this.time;
 	}
+	fromMessage = new MinHeap();
+	toMessage = new MinHeap();
 }
 
 Framework.prototype.getNodeCount = function() {
 	return this.nodeId;
 }
 
-Connection = function() {
-	messages = new MinHeap();
+Connection = function(from, to, time) {
+	this.from = from;
+	this.to = to;
+	this.time = time;
+}
+
+Message = function(from, to, sendTime, data) {
+	this.sendTime = sendTime
+	this.recvTime = null; //need to be set by framework
+	this.from = from;
+	this.to = to;
+	this.data = data;
 }
